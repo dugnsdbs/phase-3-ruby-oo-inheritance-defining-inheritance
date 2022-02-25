4a79ad4dc3599f77950be4ad90818ff74681d38e
yeohoon yoon <yhoon.yoon@gmail.com>
2022-02-25 17:51:19 -0500

    complete

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,16 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+require"pry"
+
+class Car < Vehicle
+  def global_variables
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOOOOOOM"
+  end
+
+end
+
+
+
+car1 = Car.new(20, 21)
+binding.pry
+
